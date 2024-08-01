bc6c101379d89aab5fd4b73f16990a85ccf79ba3
raceee <race.peterson@gmail.com>
2024-07-31 23:49:05 -0600

    cuda c practice

---
+++ cuda_practice/README.md
@@ -0,0 +1,3 @@
+This is lots of CUDA C exercises from the book *Programming Massively Parallel Processors* by Wen-mei W. Hwu, David B. Kirk, and Izzat El Hajj.
+
+Thank you
--- cuda_practice/main.c
+++ cuda_practice/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+void vecAdd(float* A_h, float* B_h, float* C_h, int n) {
+    for (int i = 0; i < n; ++i) {
+        C_h[i] = A_h[i] + B_h[i];
+    }
+}
+
 int main() {
-    printf("Hello, World!\n");
+    const int n = 5;
+    float A_h[5] = {1, 2, 3, 4, 5};
+    float B_h[5] = {1, 2, 3, 4, 5};
+    float C_h[5];
+
+    vecAdd(A_h, B_h, C_h, n);
+
+    for (int i = 0; i < n; ++i) {
+        printf("%f ", C_h[i]);
+    }
+    printf("\n");
+
     return 0;
 }
